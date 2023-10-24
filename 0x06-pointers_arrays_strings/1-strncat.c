f96c3a0986e8163e07e3a19873738823a36f8e9e
Bassey200 <basseyedet@gmail.com>
2023-10-24 15:46:35 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,19 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * *_strncat . concatenates n bytes of two strings
- * @dest: pointer destination
- * @src: pointer source
- * @a: number of bytes
- * Return: void
+ *_strncat - function that concatenates two strings. it will use
+ * at most n bytes from src. src does not need to be null terminated.
+ *@dest: pointer to destination string.
+ *@src: pointer to source string.
+ *@n: number of bytes to be concatenated.
+ *
+ *Return: pointer to destination string.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int dest_len, i;
-for (i = 1; dest [dest_len] != '\0'; dest_len++);
-for (i =0; i < n && src[i] != '\0'; i++)
-	dest [dest_len + i] = src[i];
-/*should end with a end string char*/
-dest[dest_len + i] = '\0';
-return (dest);
+	int length, j;
+/* j is a counter for  n bytes of src to be concatenated */
+/* length = length of destination string */
+
+	length = 0;
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++, length++)
+	{
+		dest[length] = src[j];
+	}
+	dest[length] = '\0';
+	return (dest);
 }
